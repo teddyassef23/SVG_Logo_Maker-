d1267c76c8dde79e5d2b317cdd4662d9cea29d67
Tewodros Assefa <teddyassef23@gmail.com>
2024-01-16 13:03:53 -0600

    98% chalenge is done

--- index.js
+++ index.js
@@ -7,9 +7,6 @@ var fs = require('fs');
 const { error } = require('console');
 
 const PORT = 8080;
-// Imports the graceful-fs, inquirer, Circle, Square, and Triangle modules.
-// Defines a Svg class that has a constructor with three methods for rendering and setting the text and shape elements in the SVG string.
-
 
 
 
@@ -32,8 +29,7 @@ class Svg {
 
 }
 
-// Defines array of 'questions' using the 'inquirer' library with the following questions.
-// Each question is an object that specifies the properties of TEXT, TEXT COLOR, SHAPE COLOR, and Pixel Image.
+
 const questions = [
 	{
 		type: "input",
@@ -60,18 +56,22 @@ const questions = [
 
 
 function readHtml() {
+	const PORT = 8080;
 
 	fs.readFile('./index.html', function (err, html) {
 
+		if (err) throw err;
 
+		http.createServer(function (request, response) {
+			response.writeHeader(200, { "Content-Type": "text/html" });
+			response.write(html);
+			response.end();
+		}).listen(PORT);
+	})
 
-	});
 }
 
 
-
-
-// Function to write data to file
 
 function writeToFile(fileName, data) {
 	console.log("Writing [" + data + "] to file [" + fileName + "]")
@@ -85,36 +85,34 @@ function writeToFile(fileName, data) {
 
 
 
-async function init() {
+async function create() {
 	console.log("Starting init");
 	var svgString = "";
 	var svg_file = "logo.svg";
 
-	// Prompt the user for answers
 	const answers = await inquirer.prompt(questions);
 
-	//user text
+
 	var user_text = "";
 	if (answers.text.length > 0 && answers.text.length < 4) {
-		// 1-3 chars, valid entry
+
 		user_text = answers.text;
 	} else {
-		// 0 or 4+ chars, invalid entry
+
 		console.log("Invalid user text field detected! Please enter 1-3 Characters, no more and no less");
 		return;
 	}
 	console.log("User text: [" + user_text + "]");
-	//user font color
+
 	user_font_color = answers["text-color"];
 	console.log("User font color: [" + user_font_color + "]");
-	//user shape color
+
 	user_shape_color = answers.shape;
 	console.log("User shape color: [" + user_shape_color + "]");
-	//user shape type
+
 	user_shape_type = answers["pixel-image"];
 	console.log("User entered shape = [" + user_shape_type + "]");
 
-	//user shape
 	let user_shape;
 	if (user_shape_type === "Square" || user_shape_type === "square") {
 		user_shape = new Square();
@@ -133,25 +131,23 @@ async function init() {
 	}
 	user_shape.setColor(user_shape_color);
 
-	// Create a new Svg instance and add the shape and text elements to it
+
 	var svg = new Svg();
 	svg.setTextElement(user_text, user_font_color);
 	svg.setShapeElement(user_shape);
 	svgString = svg.render();
 
-	//Print shape to log
+
 	console.log("Displaying shape:\n\n" + svgString);
-	//document.getElementById("svg_image").innerHTML = svgString;
 
 	console.log("Shape generation complete!");
 	console.log("Writing shape to file...");
 	writeToFile(svg_file, svgString);
 	readHtml();
 
-	// Tio statrt index.html file
 	http.createServer(function (request, response) {
-		response.writeHead(200,{'contain type' : 'text/plan'});
-		fs.readFile('./index.html',null, function (error, data) {
+		response.writeHead(200, { 'contain type': 'text/html' });
+		fs.readFile('./index.html', null, function (error, data) {
 
 			if (error) {
 				response.writeHead(400);
@@ -159,11 +155,13 @@ async function init() {
 				response.write(data);
 			}
 
+			response.end();
 
 		})
+		console.log("Index.html is Strarting ....")
 	});
-	
+
 
 
 }
-init()
+create()
